1ecdccef596bc3c84ac1ef7640534dc411e535cf
ruslan3lodey <ruslan3lodey@gmail.com>
2024-05-22 22:49:36 +0300

    V 0.2.0.0 docker 100%

--- app/config.py
+++ app/config.py
@@ -12,13 +12,13 @@ POSTGRES_HOST = getenv('POSTGRES_HOST')
 POSTGRES_PORT = getenv('POSTGRES_PORT')
 POSTGRES_USER = getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = getenv('POSTGRES_PASSWORD')
-POSTGRES_DB = getenv('POSTGRES_DB')
+POSTGRES_DATABASE = getenv('POSTGRES_DATABASE')
 
 DATABASE_URL = (f"postgresql+asyncpg"
                 f"://{POSTGRES_USER}"
                 f":{POSTGRES_PASSWORD}"
                 f"@{POSTGRES_HOST}"
-                f"/{POSTGRES_DB}")
+                f"/{POSTGRES_DATABASE}")
 
 UNSPLASH_ACCESS_KEY = getenv('UNSPLASH_ACCESS_KEY')
 
